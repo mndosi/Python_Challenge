90cbc05bbfcc194d17585649b8b4996e802a94bc
mndosi <mankwendosisings@gmail.com>
2022-11-21 22:23:49 -0600

    adjustments

--- PyBank/main2.py
+++ PyBank/main2.py
@@ -4,14 +4,14 @@ import os
 import csv
 from os import path
 #import csv into a path
-bank_csvpath = os.path.join("..", "PyBank", "Resources", "budget_data.csv")
-# path C:\Users\MankweNdosi\ClassActivities\Python_Challenge\PyBank\Resources\budget_data.csv
+bank_csvpath = os.path.join("Resources", "budget_data.csv")
+
 #open and read csv
-with open (bank_csvpath, 'r') as budget_file:
+with open (bank_csvpath) as budget_file:
     csv_reader = csv.reader(budget_file, delimiter = ",")
 
     # Read the header row first
-    csv_header = next(budget_file)
+    csv_header = next(csv_reader)
     print(f"Header:{ csv_header}")
 
 #read the header row first, then read through each row of the file
@@ -22,17 +22,21 @@ with open (bank_csvpath, 'r') as budget_file:
         #print([row[i] for row in result])
         #print(header_name)
         list_month.append(header_name[0])
-    print("Total months= " + str(len(list_month)))
+    print("Total months = " + str(len(list_month)))
 
 #Get the total number of months included in the dataset
-
+    total = 0
     list_profloss = []
-    for i, header_name in enumerate(result):
-        list_profloss.append(header_name[1])
-        def total_prof(list_profloss):
-            final_prof = sum(list_profloss)
-            return total_prof
-    print("Total profit for the period was " + (final_prof.str))
+    for i in range(0, len(list_profloss)):
+        list_profloss.append('Profit/Losses'[1])
+        #Iterate each element in list
+# and add them in variable total
+        total = total + list_profloss[i]
+        print("Total Profit " + total)
+ 
+# printing total value
+
+    
 
 
 # Bank_budget = ["budget_file"]
